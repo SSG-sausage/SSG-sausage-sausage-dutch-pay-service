abaf7c8192d567d9009ffa64408e55755472bf67
seoh02h <seohyunk218@gmail.com>
2023-01-14 13:52:55 +0900

    [TERK-44/Fix] DutchPayService validate 메소드 분리

--- src/main/java/com/ssg/sausagedutchpayapi/dutchpay/service/DutchPayService.java
+++ src/main/java/com/ssg/sausagedutchpayapi/dutchpay/service/DutchPayService.java
@@ -5,8 +5,6 @@ import com.ssg.sausagedutchpayapi.common.client.internal.dto.response.OrderFindC
 import com.ssg.sausagedutchpayapi.common.exception.DuplicateException;
 import com.ssg.sausagedutchpayapi.common.exception.ErrorCode;
 import com.ssg.sausagedutchpayapi.common.exception.ForbiddenException;
-import com.ssg.sausagedutchpayapi.common.exception.UnAuthorizedException;
-import com.ssg.sausagedutchpayapi.dutchpay.dto.response.DutchPayFindResponse;
 import com.ssg.sausagedutchpayapi.dutchpay.dto.response.DutchPaySaveResponse;
 import com.ssg.sausagedutchpayapi.dutchpay.entity.DutchPay;
 import com.ssg.sausagedutchpayapi.dutchpay.entity.DutchPayDtl;
@@ -14,7 +12,6 @@ import com.ssg.sausagedutchpayapi.dutchpay.repository.DutchPayDtlRepository;
 import com.ssg.sausagedutchpayapi.dutchpay.repository.DutchPayRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,17 +30,9 @@ public class DutchPayService {
         OrderFindCartShareResponse response = internalOrderApiClient.findCartShareByCartShareOrdId(
                 cartShareOrdId).getBody().getData();
 
-        if (mbrId != response.getMasterId()) {
-            throw new ForbiddenException(
-                    String.format("해당 공유 장바구니 주문(ID:%s)에 대한 함께쓱정산 생성 권한이 없습니다.", cartShareOrdId),
-                    ErrorCode.FORBIDDEN_DUTCH_PAY_CREATE_EXCEPTION);
-        }
+        validateMaster(mbrId, response.getMasterId(), cartShareOrdId);
 
-        dutchPayRepository.findByCartShareOrdId(cartShareOrdId).ifPresent(x -> {
-            throw new DuplicateException(
-                    String.format("해당 공유 장바구니 주문(ID:%s)에 대한 더치페이가 존재합니다.", cartShareOrdId),
-                    ErrorCode.DUPLICATE_DUTCH_PAY_EXCEPTION);
-        });
+        validateDuplicateDutchPay(cartShareOrdId);
 
         DutchPay dutchPay = dutchPayRepository.save(DutchPay.newInstance(cartShareOrdId));
 
@@ -53,5 +42,25 @@ public class DutchPayService {
 
         return DutchPaySaveResponse.of(dutchPay);
     }
+
+    private void validateMaster(Long mbrId, Long masterId, Long cartShareOrdId) {
+
+        if (mbrId != masterId) {
+            throw new ForbiddenException(
+                    String.format("해당 공유 장바구니 주문에 대한 함께쓱정산 생성 권한이 없습니다.", cartShareOrdId),
+                    ErrorCode.FORBIDDEN_DUTCH_PAY_CREATE_EXCEPTION);
+        }
+    }
+
+    private void validateDuplicateDutchPay(Long cartShareOrdId) {
+
+        dutchPayRepository.findByCartShareOrdId(cartShareOrdId).ifPresent(x -> {
+            throw new DuplicateException(
+                    String.format("해당 공유 장바구니 주문(ID:%s)에 대한 더치페이가 존재합니다.", cartShareOrdId),
+                    ErrorCode.DUPLICATE_DUTCH_PAY_EXCEPTION);
+        });
+
+    }
+
 
 }
