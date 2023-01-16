fb582a5923dd3da7403af0e8cb4e342d119ff91f
seoh02h <seohyunk218@gmail.com>
2023-01-16 13:27:26 +0900

    [TERK-95/Feat] Eureka 설정 추가

--- src/main/java/com/ssg/sausagedutchpayapi/SausageDutchPayApiApplication.java
+++ src/main/java/com/ssg/sausagedutchpayapi/SausageDutchPayApiApplication.java
@@ -2,13 +2,13 @@ package com.ssg.sausagedutchpayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
 @EnableJpaAuditing
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SausageDutchPayApiApplication {
 
