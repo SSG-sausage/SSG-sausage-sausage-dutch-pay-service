8dbbb4a73e9c506fbcd6a5d478126f15a948cfae
seoh02h <seohyunk218@gmail.com>
2023-01-16 13:01:56 +0900

    [TERK-95/Feat] 멤버 리스트 조회 internal api 연결 추가

---
+++ src/main/java/com/ssg/sausagedutchpayapi/common/client/internal/InternalMemberApiClient.java
@@ -0,0 +1,20 @@
+package com.ssg.sausagedutchpayapi.common.client.internal;
+
+import com.ssg.sausagedutchpayapi.common.client.internal.dto.response.MbrFindInfo;
+import com.ssg.sausagedutchpayapi.common.dto.SuccessResponse;
+import java.util.HashMap;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "InternalMemberApiClient", url = "http://localhost:8084/api")
+
+public interface InternalMemberApiClient {
+
+    @GetMapping("/member/api/mbr")
+    ResponseEntity<SuccessResponse<HashMap<Long, MbrFindInfo>>> findMbrList(
+            @RequestParam List<Long> mbrIdList);
+
+}
---
+++ src/main/java/com/ssg/sausagedutchpayapi/common/client/internal/dto/response/MbrFindInfo.java
@@ -0,0 +1,16 @@
+package com.ssg.sausagedutchpayapi.common.client.internal.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class MbrFindInfo {
+    private String mbrNm;
+
+}
